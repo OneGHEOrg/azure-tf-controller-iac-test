a1ec70a65dbbe4e0c04cddeac905083ecb4bfa77
Steven O <steveno@liatrio.com>
2024-01-03 14:28:30 -0600

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  version         = "3.85"
+  features {}
+  subscription_id = "fbd8ca72-8db1-4a6a-8b4c-773fd80183fe"
+  tenant_id       = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
+}
+
+resource "azurerm_resource_group" "test-rg" {
+  name     = "tfc-test-rg"
+  location = "westus"
+}
