0c5f52a5f000669787c52c5cb98892eee1650614
Steven O <steveno@liatrio.com>
2024-01-11 00:33:14 -0600

    adding client id

--- workloadidentity/main.tf
+++ workloadidentity/main.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
   features {}
   subscription_id           = "fbd8ca72-8db1-4a6a-8b4c-773fd80183fe"
   tenant_id                 = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
+  client_id                 = "acba7a98-6a3f-4a95-816f-b403002d9af6"
   use_aks_workload_identity = true
   use_cli                   = false
 }
