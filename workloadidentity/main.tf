2b9493f7fbcb8ad67dd8e106124600f8daf811cc
Steven O <steveno@liatrio.com>
2024-01-11 00:28:45 -0600

    testing use_aks_workload_identity

--- workloadidentity/main.tf
+++ workloadidentity/main.tf
@@ -1,9 +1,10 @@
 provider "azurerm" {
-  version         = "3.85"
+  version                   = "3.85"
   features {}
-  subscription_id = "fbd8ca72-8db1-4a6a-8b4c-773fd80183fe"
-  tenant_id       = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
-  use_oidc        = true
+  subscription_id           = "fbd8ca72-8db1-4a6a-8b4c-773fd80183fe"
+  tenant_id                 = "1b4a4fed-fed8-4823-a8a0-3d5cea83d122"
+  use_aks_workload_identity = true
+  use_cli                   = false
 }
 
 resource "azurerm_resource_group" "test-rg" {
